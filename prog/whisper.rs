95d9a15efd553cf79edee6b59d557aacf13620f5
Thomas Jespersen <laumann.thomas@gmail.com>
2014-09-05 14:40:47 +0200

    Add ![feature(macro_rules)] and convert comments

--- prog/whisper.rs
+++ prog/whisper.rs
@@ -1,26 +1,37 @@
-/// Chinese whispers in Rust
-///
-/// Start a number of processes passing along an uint. The whisper "distortion"
-/// is an increment on the passed along uint.
-///
-/// The workings can be sketched in the following way:
-///
-///          p1           p2          p3          p4
-///      +---------+ +---------+ +---------+ +---------+
-/// (tx ,|rx) - (tx|,|rx) - (tx|,|rx) - (tx|,|rx) - (tx|,|rx)
-///   ^  +---------+ +---------+ +---------+ +---------+   |
-///   |       +1          +1          +1          +1       v
-///  in                                                   out
-///
-/// Bonus instructive exercise: Try moving the `tx.send(0)` statement above the
-/// the for-loop in main(). Then processes can start passing along their data
-/// as soon as the proc is invoked.
+#![feature(macro_rules)]
+/*
+ * Chinese whispers in Rust
+ *
+ * Start a number of processes passing along an uint. The whisper "distortion"
+ * is an increment on the passed along uint.
+ *
+ * The workings can be sketched in the following way:
+ *
+ *          p1           p2          p3          p4
+ *      +---------+ +---------+ +---------+ +---------+
+ * (tx ,|rx) - (tx|,|rx) - (tx|,|rx) - (tx|,|rx) - (tx|,|rx)
+ *   ^  +---------+ +---------+ +---------+ +---------+   |
+ *   |       +1          +1          +1          +1       v
+ *  in                                                   out
+ *
+ * Bonus instructive exercise: Try moving the `tx.send(0)` statement above the
+ * the for-loop in main(). Then processes can start passing along their data
+ * as soon as the proc is invoked.
+ */
+
 extern crate getopts;
 
 use getopts::{optopt,optflag,getopts,usage};
 use std::os;
 
 static NPROC_DEFAULT: uint = 25_000;
+
+/*
+ * Short-hand macro for spawn(proc() ...)
+ */
+macro_rules! go(
+    ($e:expr) => (spawn(proc() $e))
+)
 
 fn whisper(rx: Receiver<uint>, tx: Sender<uint>) {
     let n = rx.recv();
@@ -39,7 +50,9 @@ fn main() {
 
     for _ in range(0, nproc) {
         let (tx_next, rx_next) = channel();
-        spawn(proc() whisper(rx, tx_next));
+        go! {
+            whisper(rx, tx_next)
+        };
         rx = rx_next;
     }
 
@@ -49,18 +62,21 @@ fn main() {
 }
 
 
-/// Handle command-line arguments
-/// 
-/// Return the number of processes to start and whether or not parsing was
-/// successful.
-/// 
-/// The tuple returned is inspired by Go's multiple return values (this in
-/// essence just models that). I suppose in idiomatic Rust, one should return
-/// an Option<uint> indicating whether or not a number could be found.
-///
-/// This code was split into its own function, because it started to dominate
-/// the code that is actually interesting here. This code should just provide
-/// the option to specify the number of processes to start.
+
+/* 
+ * Handle command-line arguments
+ * 
+ * Return the number of processes to start and whether or not parsing was
+ * successful.
+ * 
+ * The tuple returned is inspired by Go's multiple return values (this in
+ * essence just models that). I suppose in idiomatic Rust, one should return
+ * an Option<uint> indicating whether or not a number could be found.
+ *
+ * This code was split into its own function, because it started to dominate
+ * the code that is actually interesting here. This code should just provide
+ * the option to specify the number of processes to start.
+ */
 fn handle_args() -> (uint, bool) {
     let args = os::args();
     let prog = args[0].clone();
