73e9de006cbbe4e6a1cc8e93deaed68e18fdef93
Thomas Jespersen <laumann.thomas@gmail.com>
2014-12-30 10:49:56 +0100

    Update to new threads

--- prog/channels.rs
+++ prog/channels.rs
@@ -1,6 +1,7 @@
 /// Simple channels example
 ///
 ///
+use std::thread::Thread;
 
 static NTASKS: uint = 10;
 
@@ -10,10 +11,10 @@ fn main() {
     for _ in range(0, NTASKS) {
 	let (tx, rx) = channel();
 	senders.push(tx);
-	spawn(move|| {
+	Thread::spawn(move|| {
 	    let n = rx.recv();
 	    println!("task received {}", n);
-	});
+	}).detach();
     }
 
     for i in range(0, NTASKS) {
