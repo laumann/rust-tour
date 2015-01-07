af484c939c134baacad7fca6e32c199d4f0ce42b
Thomas Jespersen <laumann.thomas@gmail.com>
2015-01-07 14:23:04 +0100

    Fix channels.rs

--- prog/channels.rs
+++ prog/channels.rs
@@ -1,22 +1,31 @@
 /// Simple channels example
 ///
-///
+/// Start ten channels
+use std::thread::Thread;
+use std::sync::mpsc::channel;
 
 static NTASKS: uint = 10;
 
 fn main() {
-    let mut senders = Vec::new();
+    let mut txs = Vec::with_capacity(NTASKS);
+    let mut threads = Vec::with_capacity(NTASKS);
 
     for _ in range(0, NTASKS) {
-	let (tx, rx) = channel();
-	senders.push(tx);
-	spawn(move|| {
-	    let n = rx.recv();
-	    println!("task received {}", n);
-	});
+        let (tx, rx) = channel();
+        txs.push(tx);
+
+        let join_guard = Thread::scoped(move|| {
+            let i = rx.recv().unwrap();
+            println!("{}", rx.recv().unwrap());
+        });
+        threads.push(join_guard);
     }
 
-    for i in range(0, NTASKS) {
-	senders[i].send(i);
+    for (i, tx) in txs.iter().enumerate() {
+        tx.send(i).unwrap();
+    }
+
+    for t in threads.into_iter() {
+        t.join();
     }
 }
